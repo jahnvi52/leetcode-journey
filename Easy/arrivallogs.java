eb9b2d88d70b8e9b30c68bc6859c41b448edf5f9
jahnvi52 <jahnvishukla55@gmail.com>
2026-07-09 22:02:03 +0530

    recursive fibonacci

--- Easy/arrivallogs.java
+++ Easy/arrivallogs.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class arrayreversal {
+public class arrivallogs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int p = sc.nextInt();
