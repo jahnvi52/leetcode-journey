3d0433f8b753ecd65dd58e711c460bf30d1b4475
jahnvi52 <jahnvishukla55@gmail.com>
2026-07-10 16:53:59 +0530

    add the matrix

---
+++ Easy/matrixadditon.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class matrixadditon {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] A = new int[m][n];
+        int[][] B = new int[m][n];
+        int[][] C = new int[m][n];
+
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
