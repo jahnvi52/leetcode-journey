cef3f46d9bca9e2be44bf1dc8217bb69ac21a0ca
jahnvi52 <jahnvishukla55@gmail.com>
2026-07-10 12:10:44 +0530

    reverse string

---
+++ Easy/reversestring.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class reversestring {
+
+    static void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        char[] s = str.toCharArray();
+
+        reverseString(s);
+
+        System.out.println(s);
+
+        sc.close();
+    }
+}
