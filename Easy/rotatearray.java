baf0819ca59629717cced2f5870df82a71fade2d
jahnvi52 <jahnvishukla55@gmail.com>
2026-07-13 20:43:26 +0530

    rotate the array

---
+++ Easy/rotatearray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class rotatearray {
+
+    static void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+
+        k = k % n;
+
+        int[] temp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int nums[] = {1,2,3,4,5,6,7};
+
+        rotate(nums,3);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
