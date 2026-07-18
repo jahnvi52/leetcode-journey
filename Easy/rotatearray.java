794fb5e7d189d3cab9c9afb28bb1485db86bee03
jahnvi52 <jahnvishukla55@gmail.com>
2026-07-18 18:57:05 +0530

    maximum subarray sum circular

--- Easy/rotatearray.java
+++ Easy/rotatearray.java
@@ -21,9 +21,9 @@ public class rotatearray {
 
     public static void main(String[] args) {
 
-        int nums[] = {1,2,3,4,5,6,7};
+        int nums[] = {1,2,3,4,5};
 
-        rotate(nums,3);
+        rotate(nums,2);
 
         System.out.println(Arrays.toString(nums));
     }
