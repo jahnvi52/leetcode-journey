1cc80a93d5b24cbc588bd044435a423db15268b2
jahnvi52 <jahnvishukla55@gmail.com>
2026-07-19 01:03:42 +0530

    middle linked list

---
+++ Easy/middlelinkedlist.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class middlelinkedlist {
+
+    public static Node findMiddle(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < n; i++) {
+
+            int value = sc.nextInt();
+
+            Node newNode = new Node(value);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        Node middle = findMiddle(head);
+
+        System.out.println(middle.data);
+
+        sc.close();
+    }
+}
