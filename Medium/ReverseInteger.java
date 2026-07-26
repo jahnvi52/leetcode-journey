5d91c6e3450663d924dd75434ed047625eab652c
jahnvi52 <jahnvishukla55@gmail.com>
2026-07-26 09:06:25 +0530

    reverse the integer

---
+++ Medium/ReverseInteger.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ReverseInteger {
+
+    public static int reverse(int x) {
+
+        int rev = 0;
+
+        while (x != 0) {
+
+            int digit = x % 10;
+
+            // Check positive overflow
+            if (rev > Integer.MAX_VALUE / 10 ||
+               (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+
+            // Check negative overflow
+            if (rev < Integer.MIN_VALUE / 10 ||
+               (rev == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+
+            x = x / 10;
+        }
+
+        return rev;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Integer: ");
+        int x = sc.nextInt();
+
+        int ans = reverse(x);
+
+        System.out.println("Reversed Integer = " + ans);
+
+        sc.close();
+    }
+}
